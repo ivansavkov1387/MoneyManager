2bcb7851abdacb47edf824615137e11676a9e5e3
Ivan Savkov <ivansavkov1387@gmail.com>
2021-10-21 14:50:34 +0300

    Finish UI and start keyboard logic

--- MoneyManager/ViewController.swift
+++ MoneyManager/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var stillTyping = false
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet var keyboardButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        
+        let number = sender.currentTitle!
+        
+        if stillTyping {
+            if displayLabel.text!.count < 15 {
+                displayLabel.text = displayLabel.text! + number
+            }
+        } else {
+            displayLabel.text = number
+            stillTyping = true
+        }
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        displayLabel.text = "0"
+        stillTyping = false
+    }
+    
+    
 }
 
